772bb5d9974f565b77bbc058930f6a30e6e8ddb0
ted0000 <ted.kim@obigo.com>
2012-11-29 16:40:34 +0900

    add npm module

--- win/bin/node/www/index.js
+++ win/bin/node/www/index.js
@@ -1,9 +1,19 @@
-var express=require('express'),app=express();
+var express=require('express');
+var app=express();
+var fs = require('fs');
 
 app.configure( function(){
   app.use(express.methodOverride());
-  app.use(express.static(__dirname+''));
+  app.use(express.static(__dirname+ ''));
   app.use(app.router);
 });
+
+app.get(/getVideoList/, function(req,res){
+	console.log('connected getVideoList');
+	//res.write("1");  
+	res.end();  
+});
+
+console.log('Express server start');
 
 app.listen(8888);
