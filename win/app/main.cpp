5278060748b39eb3180629008c7e4830ea896236
unknown <krinedse@krsepc864.ac.obigo.com>
2013-01-22 16:58:53 +0900

    implement Node.js start/stop button in WINAPP

--- win/app/main.cpp
+++ win/app/main.cpp
@@ -1,11 +1,18 @@
-#include "appview.h"
+#include "mainwindow.h"
 #include <QApplication>
+
+/* File     : main.cpp
+ * Author   : Edgar Seo
+ * Company  : OBIGO KOREA
+ * Version  : 1.0
+ * Date     : 2013-01-15
+ */
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    AppView w;
+    MainWindow w;
     w.show();
-
+    
     return a.exec();
 }
