237c5f07e9a907c48c1751ad0e426cdaa23a3d5d
iamgood <youngju.hong@obigo.com>
2013-01-29 19:25:09 +0900

    updadte video-play for TV and Android and IPhone

--- win/bin/node/public/app.js
+++ win/bin/node/public/app.js
@@ -7,8 +7,10 @@ var musicmetadata = require('musicmetadata');
 var confxmlPath = __dirname + "/../../conf.xml";
 var videoFileExt = [".avi", ".mp4"];
 var audioFileExt = [".mp3"];
+var cacheDirectoryVideo = "cache/video";
 var virtualDirectoryVideo = "__vd__video";
-var videoCacheDir = "cache/video";
+var virtualRedirectNormal = "__vr__normal";
+var virtualRedirectAndroid = "__vr__android";
 
 app.configure( function(){
   app.use(express.bodyParser());
@@ -153,118 +155,6 @@ function makeAudioMetaData(){
 	});
 }
 
-app.get('/getVideoList', function(req,res){
-	var rtn = [];
-	var parser = new xml2js.Parser();	//xml2js parser
-	fs.readFile(confxmlPath, function(err, data) {
-	    parser.parseString(data, function (err, result) {	//xml2js parse	        
-	    	for(var i=0; i<result.shareddir.contents.length; i++){
-	        	rtn = rtn.concat( getList( String(result.shareddir.contents[i].lnpath), videoFileExt, "v", virtualDirectoryVideo+"/") );
-	    	}
-		    var returnJson = JSON.stringify(rtn.sort(comp));
-		    if(returnJson.length > 0)
-		    	res.end(returnJson);
-	    });
-	});
-});
-
-app.post('/getVideoThumbnail', function(req, res){
-	var tmpPath = req.body.path;
-	var reqVideoId = req.body.selectedVideoId;
-	var item = new Object();
-	item.selectedVideoId = reqVideoId;
-	item.picture = "";
-
-	if(tmpPath) {
-		var reqVideoPath = "/contents" + tmpPath.substring(tmpPath.indexOf(virtualDirectoryVideo)+virtualDirectoryVideo.length);
-		var thumnailFile = __dirname + "/" + videoCacheDir + reqVideoPath + ".jpg";
-		thumnailFile = thumnailFile.replace(/\//g, '\\');  //same-code: thumnailFile = thumnailFile.split("/").join("\\");
-		var thumnailPath = thumnailFile.substring(0, thumnailFile.lastIndexOf('\\'));
-
-		//check a previous thumbnail file.
-		if(fs.existsSync(thumnailFile)) {
-			var thumbNailData = fs.createReadStream(thumnailFile, {flags: 'r', encoding: 'base64', bufferSize: 8192 });
-			thumbNailData.on('data', function(d) {
-				//YOHO:Error - Do not read and append small buffer data. Currently it makes data length error.
-				//I don't know why, maybe nodejs fs module bugs!!!. If you avoid this problem,
-				//keep bufferSize 2-times larger than jpg file size to read all data of a thumbnail file at a time.
-				//base64 encoded size: if original data is 55 bytes, the encoded size is (55/3) * 4 = 19 * 4 = 76 bytes.
-				//about 30% increasing.
-				item.picture = item.picture + d;
-			});
-			thumbNailData.on('error', function(e) {
-				item.picture = "";
-			});
-			thumbNailData.on('close', function() {
-				res.end(JSON.stringify(item));
-				console.log("Screenshots: "+thumnailFile.substring(thumnailFile.lastIndexOf('\\')+1)+" was replied.");
-			});
-		}
-		else {
-			// make a directory for thumbnail files.
-			fs.mkdirRecursiveSync(thumnailPath);
-
-			// make sure you set the correct path to your video file storage
-			var pathToMovie = __dirname + reqVideoPath;
-			pathToMovie = pathToMovie.replace(/\//g, '\\');
-
-			//check a requested video file.
-			if(fs.existsSync(pathToMovie)) {
-				var proc = new ffmpeg({
-					source: pathToMovie,  // input source, required
-					timeout: 300*60, // timout of the spawned ffmpeg sub-processes in seconds (optional, defaults to 30)
-					priority: 0,          // default priority for all ffmpeg sub-processes (optional, defaults to 0 which is no priorization)
-					logger: null,        // set a custom [winston](https://github.com/flatiron/winston) logging instance (optional, default null which will cause fluent-ffmpeg to spawn a winston console logger)
-					nolog: false        // completely disable logging (optional, defaults to false)
-				})
-				.withSize('128x128')
-				// take 2 screenshots at predefined timemarks
-				//.takeScreenshots({ count: 2, timemarks: [ '50%', '75%' ], filename: '%f' }, thumnailPath, function(error, filenames) {
-				//.takeScreenshots({ count: 2, timemarks: [ '0.5', '1.0' ], filename: '%f' }, thumnailPath, function(error, filenames) {
-
-				// take 1 screenshots at predefined timemarks
-				.takeScreenshots({ count: 1, timemarks: [ '1.0' ], filename: '%f' }, thumnailPath, function(error, filenames) {
-					console.log("----Start-Msg-------------------------------------");
-					if(error) {
-						console.log("Screenshots: " + error);
-					}
-					else {
-						console.log("Screenshots: "+filenames+" was saved.");
-					}
-					console.log("----End-Msg---------------------------------------");
-					if(filenames) {
-						var thumbNailData = fs.createReadStream(thumnailFile, {flags: 'r', encoding: 'base64', bufferSize: 8192 });
-						thumbNailData.on('data', function(d) {
-							//YOHO:Error - Do not read and append small buffer data. Currently it makes data length error.
-							//I don't know why, maybe nodejs fs module bugs!!!. If you avoid this problem,
-							//keep bufferSize 2-times larger than jpg file size to read all data of a thumbnail file at a time.
-							//base64 encoded size: if original data is 55 bytes, the encoded size is (55/3) * 4 = 19 * 4 = 76 bytes.
-							//about 30% increasing.
-							item.picture = item.picture + d;
-						});
-						thumbNailData.on('error', function(e) {
-							item.picture = "";
-						});
-						thumbNailData.on('close', function() {
-							res.end(JSON.stringify(item));
-							console.log("Screenshots: "+filenames+" was replied.");
-							//fs.unlinkSync(thumnailFile);
-							//console.log("Screenshots: "+filenames+" was deleted after using.");
-						});
-					}
-					else {
-						res.end(JSON.stringify(item));
-					}
-				});
-			}
-			else {
-				res.end(JSON.stringify(item));
-				console.log("Screenshots: there is no viedo file at " + pathToMovie);
-			}
-		}
-	}
-});
-
 app.get('/getAudioList', function(req,res){
 	prepairMetadata();
 	
@@ -332,42 +222,263 @@ app.get('/getDropboxList', function(req,res){
 	});
 });
 
+app.get('/getVideoList', function(req,res){
+	var rtn = [];
+	var parser = new xml2js.Parser();	//xml2js parser
+	fs.readFile(confxmlPath, function(err, data) {
+	    parser.parseString(data, function (err, result) {	//xml2js parse	        
+	    	for(var i=0; i<result.shareddir.contents.length; i++){
+	        	rtn = rtn.concat( getList( String(result.shareddir.contents[i].lnpath), videoFileExt, "v", virtualDirectoryVideo+"/") );
+	    	}
+		    var returnJson = JSON.stringify(rtn.sort(comp));
+		    if(returnJson.length > 0)
+		    	res.end(returnJson);
+	    });
+	});
+});
+
+app.post('/getVideoThumbnail', function(req, res){
+	var tmpPath = req.body.path;
+	var reqVideoId = req.body.selectedVideoId;
+	var item = new Object();
+	item.selectedVideoId = reqVideoId;
+	item.picture = "";
+
+	if(tmpPath) {
+		var reqVideoPath = "/contents" + tmpPath.substring(tmpPath.indexOf(virtualDirectoryVideo)+virtualDirectoryVideo.length);
+		var thumnailFile = __dirname + "/" + cacheDirectoryVideo + reqVideoPath + ".jpg";
+		thumnailFile = thumnailFile.replace(/\//g, '\\');  //same-code: thumnailFile = thumnailFile.split("/").join("\\");
+		var thumnailPath = thumnailFile.substring(0, thumnailFile.lastIndexOf('\\'));
+
+		//check a previous thumbnail file.
+		if(fs.existsSync(thumnailFile)) {
+			var thumbNailData = fs.createReadStream(thumnailFile, {flags: 'r', encoding: 'base64', bufferSize: 8192 });
+			thumbNailData.on('data', function(d) {
+				//YOHO:Error - Do not read and append small buffer data. Currently it makes data length error.
+				//I don't know why, nodejs fs module bugs???. To avoid this problem,
+				//keep bufferSize 2-times larger than jpg file size to read all data of a thumbnail file at a time.
+				//base64 encoded size: if original data is 55 bytes, the encoded size is (55/3) * 4 = 19 * 4 = 76 bytes.
+				//about 30% increasing.
+				item.picture = item.picture + d;
+			});
+			thumbNailData.on('error', function(e) {
+				item.picture = "";
+			});
+			thumbNailData.on('close', function() {
+				res.end(JSON.stringify(item));
+				console.log("Screenshots: "+thumnailFile.substring(thumnailFile.lastIndexOf('\\')+1)+" was replied.");
+			});
+		}
+		else {
+			// make a directory for thumbnail files.
+			fs.mkdirRecursiveSync(thumnailPath);
+
+			// make sure you set the correct path to your video file storage
+			var pathToMovie = __dirname + reqVideoPath;
+			pathToMovie = pathToMovie.replace(/\//g, '\\');
+
+			//check a requested video file.
+			if(fs.existsSync(pathToMovie)) {
+				var proc = new ffmpeg({
+					source: pathToMovie,  // input source, required
+					timeout: 300*60, // timout of the spawned ffmpeg sub-processes in seconds (optional, defaults to 30)
+					priority: 0,          // default priority for all ffmpeg sub-processes (optional, defaults to 0 which is no priorization)
+					logger: null,        // set a custom [winston](https://github.com/flatiron/winston) logging instance (optional, default null which will cause fluent-ffmpeg to spawn a winston console logger)
+					nolog: false        // completely disable logging (optional, defaults to false)
+				})
+				.withSize('128x128')
+				// take 2 screenshots at predefined timemarks(50% and 1.0 sec)
+				//.takeScreenshots({ count: 2, timemarks: [ '50%', '1.0' ], filename: '%f' }, thumnailPath, function(error, filenames) {
+				// take 1 screenshots at predefined timemarks
+				.takeScreenshots({ count: 1, timemarks: [ '50%' ], filename: '%f' }, thumnailPath, function(error, filenames) {
+					if(error) {
+						console.log("Screenshots: " + error);
+					}
+					else {
+						console.log("Screenshots: "+filenames+" was saved.");
+					}
+					if(filenames) {
+						var thumbNailData = fs.createReadStream(thumnailFile, {flags: 'r', encoding: 'base64', bufferSize: 8192 });
+						thumbNailData.on('data', function(d) {
+							//YOHO:Error - Do not read and append small buffer data. Currently it makes data length error.
+							//I don't know why, nodejs fs module bugs???. To avoid this problem,
+							//keep bufferSize 2-times larger than jpg file size to read all data of a thumbnail file at a time.
+							//base64 encoded size: if original data is 55 bytes, the encoded size is (55/3) * 4 = 19 * 4 = 76 bytes.
+							//about 30% increasing.
+							item.picture = item.picture + d;
+						});
+						thumbNailData.on('error', function(e) {
+							item.picture = "";
+						});
+						thumbNailData.on('close', function() {
+							res.end(JSON.stringify(item));
+							console.log("Screenshots: "+filenames+" was replied.");
+							//fs.unlinkSync(thumnailFile);
+							//console.log("Screenshots: "+filenames+" was deleted after using.");
+						});
+					}
+					else {
+						res.end(JSON.stringify(item));
+					}
+				});
+			}
+			else {
+				res.end(JSON.stringify(item));
+				console.log("Screenshots: there is no viedo file at " + pathToMovie);
+			}
+		}
+	}
+});
+
 app.get("/"+virtualDirectoryVideo+"/*", function(req, res) {
 	// make sure you set the correct path to your video file storage
 	var pathToMovie = __dirname + '/contents/' + req.params[0];
 	pathToMovie = pathToMovie.replace(/\//g, '\\');
+	//-------------------------------------------------------------------
+	var tsFile = cacheDirectoryVideo + '/contents/' + req.params[0] + ".ts";
+	var outputFile = __dirname + '/' + tsFile;
+	outputFile = outputFile.replace(/\//g, '\\');
+	var outputPath = outputFile.substring(0, outputFile.lastIndexOf('\\'));
+	//-------------------------------------------------------------------
+	var userAgent = req.headers["user-agent"];
+	var androidStringIndex = userAgent.search(/Android/i);
+
+	if(androidStringIndex != -1) {
+		var androidString = userAgent.substring(androidStringIndex);
+		var semicolonIndex = androidString.indexOf(';');
+		if(semicolonIndex != -1) {
+			androidString = androidString.substring(0, semicolonIndex);
+		}
+		var versionIndex = androidString.search(/ [0-9].[0-9]/i);
+		if(versionIndex != -1) {
+			androidString = androidString.substring(versionIndex+1);
+			var androidVersion = parseInt(androidString);
+		}
+		else {
+			// There is no version but we assume that it is more than 4.0.0.
+			var androidVersion = 4;
+		}
+		if(androidVersion < 4) {
+			res.send('Sorry, android version('+androidString+') of your browser is too low to support.<br/>At least, it needs more then Android 4.0.0');
+			console.log("Video-File(A): Sorry, android version("+androidString+") of your browser is too low to support.\r\nAt least, it needs more then Android 4.0.0");
+			return;
+		}
+	}
+
+	if(fs.existsSync(outputFile)) {
+		if(androidStringIndex != -1) {
+			res.redirect(virtualRedirectAndroid+'/'+tsFile);
+		}
+		else {
+			res.redirect(virtualRedirectNormal+'/'+tsFile+'.m3u8');
+		}
+	}
+	else {
+		// check if the requested file is being prepared or was prepared.
+		if(fs.existsSync(outputFile+'.preparing')) {
+			// watch if outputFile is created. and then this request is redirected.
+			fs.watchFile(outputFile, function (curr, prev) {
+				console.log("Video-File: "+tsFile+' is preparing.');
+				fs.unwatchFile(outputFile);
+				if(androidStringIndex != -1) {
+					res.redirect(virtualRedirectAndroid+'/'+tsFile);
+				}
+				else {
+					res.redirect(virtualRedirectNormal+'/'+tsFile+'.m3u8');
+				}
+			});
+		}
+		else {
+			// from now, even though this request is disconnected or being disconnected
+			// the server application(app.js) will keep to covert for next other client requests.
+
+			// make a directory for output files.
+			fs.mkdirRecursiveSync(outputPath);
+
+			// touch *.preparing file to mark that the requested file is being prepared or was prepared.
+			fs.closeSync(fs.openSync(outputFile+'.preparing', 'a'));
+
+			// watch if outputFile is created. and then this request is redirected.
+			fs.watchFile(outputFile, function (curr, prev) {
+				console.log("Video-File: "+tsFile+' is preparing.');
+				fs.unwatchFile(outputFile);
+				if(androidStringIndex != -1) {
+					res.redirect(virtualRedirectAndroid+'/'+tsFile);
+				}
+				else {
+					res.redirect(virtualRedirectNormal+'/'+tsFile+'.m3u8');
+				}
+			});
+
+			//check a requested video file.
+			if(fs.existsSync(pathToMovie)) {
+				//res.contentType('m3u8');	//res.contentType('mp4');
+				var proc = new ffmpeg({
+					source: pathToMovie,  // input source, required
+					timeout: 300*60, // timout of the spawned ffmpeg sub-processes in seconds (optional, defaults to 30)
+					priority: 0,          // default priority for all ffmpeg sub-processes (optional, defaults to 0 which is no priorization)
+					logger: null,        // set a custom [winston](https://github.com/flatiron/winston) logging instance (optional, default null which will cause fluent-ffmpeg to spawn a winston console logger)
+					nolog: false        // completely disable logging (optional, defaults to false)
+				})
+				.toFormat('mpegts')  //.toFormat('mp4')
+				.withVideoBitrate('480k')
+				.withVideoCodec('libx264')
+				.withSize('480x320')
+				.withAudioBitrate('64k')
+				.withAudioCodec('libmp3lame')  //.withAudioCodec('aac')  //.withAudioCodec('libvo_aacenc')
+				.addOptions(['-bt 200k', '-subq 7', '-me_range 16', '-qcomp 0.6', '-qmin 10', '-qmax 51'])
+				//.addOptions(['-strict -2 -bt 200k', '-subq 7', '-me_range 16', '-qcomp 0.6', '-qmin 10', '-qmax 51'])
+				// save to stream
+				//.writeToStream(res, function(retcode, error){
+				// save to file
+				.saveToFile(outputFile, function(retcode, error){
+					if(error) {
+						console.log("Video-File: " + error);
+					}
+					console.log("Video-File: " + req.params[0] + " has been converted succesfully.");
+				});
+			}
+			else {
+				res.send(404, 'Sorry, your video list may be out-of-date.<br/>Reload the video list and then use it.');
+				console.log("Video-File: there is no file at " + pathToMovie);
+			}
+		}
+	}
+});
+
+app.get("/"+virtualRedirectAndroid+"/*", function(req, res) {
+	var tsFile = req.params[0];
+	var requestedFile = __dirname + '/' + tsFile;
+	requestedFile = requestedFile.replace(/\//g, '\\');
 
 	//check a requested video file.
-	if(fs.existsSync(pathToMovie)) {
+	if(fs.existsSync(requestedFile)) {
 		res.contentType('m3u8');
 		var proc = new ffmpeg({
-			source: pathToMovie,  // input source, required
-			timeout: 300*60, // timout of the spawned ffmpeg sub-processes in seconds (optional, defaults to 30)
-			priority: 0,          // default priority for all ffmpeg sub-processes (optional, defaults to 0 which is no priorization)
-			logger: null,        // set a custom [winston](https://github.com/flatiron/winston) logging instance (optional, default null which will cause fluent-ffmpeg to spawn a winston console logger)
-			nolog: false        // completely disable logging (optional, defaults to false)
+			source: requestedFile, timeout: 300*60, priority: 0, logger: null, nolog: false
 		})
-		.toFormat('mpegts')
-		.withVideoBitrate('480k')
-		.withVideoCodec('libx264')
-		.withSize('480x320')
-		.withAudioBitrate('128k')
-		.withAudioCodec('libmp3lame')
-		.addOptions(['-bt 200k', '-subq 7', '-me_range 16', '-qcomp 0.6', '-qmin 10', '-qmax 51'])
+		.addOptions(['-vcodec copy','-acodec copy','-f mpegts'])
 		// save to stream
 		.writeToStream(res, function(retcode, error){
-			console.log("----Start-Msg-------------------------------------");
 			if(error) {
-				//console.log("Video-File: " + error);
+				console.log("Video-File(A): " + error);
 			}
-			console.log("Video-File: " + req.params[0] + " has been converted succesfully.");
-			console.log("----End-Msg---------------------------------------");
+			console.log("Video-File(A): " + req.params[0] + " has been converted succesfully.");
 		});
 	}
 	else {
-		res.send(404, 'Sorry, your video list may be out-of-date. reload the video list and then use it.');
-		console.log("Video-File: there is no file at " + pathToMovie);
+		res.send(404, 'Sorry, your video list may be out-of-date.<br/>Reload the video list and then use it.');
+		console.log("Video-File(A): there is no file at " + requestedFile);
 	}
+});
+
+app.get("/"+virtualRedirectNormal+"/*", function(req, res) {
+	var requestedFile = req.params[0];
+	var tsFile = requestedFile.substring(0, requestedFile.lastIndexOf('.'));
+	//var m3u8File = "#EXTM3U\r\n#EXT-X-TARGETDURATION:18000\r\n#EXTINF:18000,\r\n"+"http://"+req.headers.host+"/"+tsFile+"\r\n#EXT-X-ENDLIST\r\n";
+	var m3u8File = "#EXTM3U\r\n#EXTINF:18000,\r\n"+"http://"+req.headers.host+"/"+tsFile+"\r\n";
+	res.contentType('m3u8');
+	res.send(m3u8File);
 });
 
 console.log('Make the audio metadata db...');
